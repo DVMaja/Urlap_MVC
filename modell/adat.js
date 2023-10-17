2c4e51555dd65612979d37da7ff27e308e911581
DVMaja <majadreilinger@gmail.com>
2023-10-17 09:03:27 +0200

    get leiro

--- modell/adat.js
+++ modell/adat.js
@@ -1,3 +1,22 @@
-export default urlapleiro={
-    
+export const urlapleiro = {//objektum
+    nev: {
+        megj: "Név",
+        type: "text",
+        placeholder: "Valaki Vagyok",
+        value: "",
+        regex: "[A-Z][a-z]{3}"   //regex
+
+    },
+
+    szul: {
+        megj: "Születési idő",
+        type: "number",
+        placeholder: "200",
+        value: "200",
+        regex: {
+            min: 1000,
+            max: 2023,
+        }
+
+    }
 }
