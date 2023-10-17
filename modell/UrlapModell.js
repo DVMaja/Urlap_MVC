400fc7c84d2038d04722d1f34f6699c7f3d9a2cc
DVMaja <majadreilinger@gmail.com>
2023-10-17 09:03:39 +0200

    javítás

--- modell/UrlapModell.js
+++ modell/UrlapModell.js
@@ -1,6 +1,20 @@
-class UrlapModell {
-    constructor() {
+import { urlapleiro } from "./adat.js";
 
+class UrlapModell {
+
+    #leiro;
+
+    constructor() {
+        this.#leiro=urlapleiro;
+    }
+
+    getLeiro(){
+        return this.#leiro;
+    }
+
+    //mostantól ezt használjuk.
+    get leiro(){
+        return this.#leiro;
     }
 }
 export default UrlapModell;
