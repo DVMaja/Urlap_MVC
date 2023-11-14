2d2e6b47bc1fc7f0b3593fb135f847786d51edc7
DVMaja <majadreilinger@gmail.com>
2023-11-14 09:03:14 +0100

    aktualizálás

--- modell/adat/DataService.js
+++ modell/adat/DataService.js
@@ -11,12 +11,12 @@ class DataService {
             .then(function (response) {
                 // handle success               
                 console.log("Válasz objektum: ", response);
-                console.log("Adatok: ", response.data.nevek);
+                console.log("Adatok: ", response.data);
                 console.log("Státusz: ", response.status);
                 console.log("Státusz szöveg: ", response.statusText);
                 console.log("Válasz fejléc: ", response.headers);
                 console.log("Válasz config: ", response.config); 
-                callback(response.data.nevek);
+                callback(response.data);
             })
             .catch(function (error) {
                 // handle error
